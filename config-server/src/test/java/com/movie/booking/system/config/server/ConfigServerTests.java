306b86aa8b2a540bb9a0bbfab072b159b49f7439
nitingupta3 <nitin.gupta3@bajajfinserv.in>
2023-07-11 15:48:23 +0530

    config server running..

---
+++ config-server/src/test/java/com/movie/booking/system/config/server/ConfigServerTests.java
@@ -0,0 +1,12 @@
+package com.movie.booking.system.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigServerTests {
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ discovery-server/src/test/java/com/movie/booking/service/discovery/DiscoveryServerTests.java
@@ -0,0 +1,11 @@
+package com.movie.booking.service.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DiscoveryServerTests {
+    @Test
+    void contextLoads() {
+    }
+}
